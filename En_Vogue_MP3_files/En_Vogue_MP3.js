3d3477f2d1de9f515c5731972bec6f27552b30e3
sgravois <smgravois@gmail.com>
2016-01-05 23:07:24 -0800

    new en vogue page

--- En_Vogue_MP3_files/En_Vogue_MP3.js
+++ En_Vogue_MP3_files/En_Vogue_MP3.js
@@ -1,2 +1,23 @@
 // Created by iWeb 3.0.1 local-build-20160105
 
+function writeMovie1() {
+  document.write('<audio id="id2" style="width:300px" src="Media/Never%20Gonna%20get%20it.wav" preload="auto" controls><p>Your browser does not support the <code>audio</code> element.</p></audio>');
+}
+
+/*
+function writeMovie1()
+{detectBrowser();if(windowsInternetExplorer)
+{document.write('<object id="id2" classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" codebase="http://www.apple.com/qtactivex/qtplugin.cab" width="300" height="16" style="height: 16px; left: 200px; position: absolute; top: 428px; width: 300px; z-index: 1; "><param name="src" value="Media/Never%20Gonna%20get%20it.wav" /><param name="controller" value="true" /><param name="autoplay" value="false" /><param name="scale" value="tofit" /><param name="volume" value="100" /><param name="loop" value="false" /></object>');}
+else if(isiPhone)
+{document.write('<object id="id2" type="video/quicktime" width="300" height="16" style="height: 16px; left: 200px; position: absolute; top: 428px; width: 300px; z-index: 1; "><param name="src" value="Media/Never%20Gonna%20get%20it.wav"/><param name="controller" value="true"/><param name="scale" value="tofit"/></object>');}
+else
+{document.write('<object id="id2" type="video/quicktime" width="300" height="16" data="Media/Never%20Gonna%20get%20it.wav" style="height: 16px; left: 200px; position: absolute; top: 428px; width: 300px; z-index: 1; "><param name="src" value="Media/Never%20Gonna%20get%20it.wav"/><param name="controller" value="true"/><param name="autoplay" value="false"/><param name="scale" value="tofit"/><param name="volume" value="100"/><param name="loop" value="false"/></object>');}}
+*/
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('En_Vogue_MP3_files/En_Vogue_MP3Moz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
